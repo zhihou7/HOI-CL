5c4b4228602a85a504e53c913dcab4099c021b65
zhihou7 <zhou9878@uni.sydney.edu.au>
2021-05-17 22:39:28 +0800

    add gt box and vcl box into training data

--- misc/download_training_data.sh
+++ misc/download_training_data.sh
@@ -6,6 +6,9 @@ python lib/ult/Download_data.py 1z5iZWJsMl4hafo0u7f1mVj7A2S5HQBOD Data/action_in
 python lib/ult/Download_data.py 1QeCGE_0fuQsFa5IxIOUKoRFakOA87JqY Data/prior_mask.pkl
 python lib/ult/Download_data.py 1JRMaE35EbJYxkXNSADEgTtvAFDdU20Ru Data/Test_Faster_RCNN_R-50-PFN_2x_HICO_DET.pkl
 python lib/ult/Download_data.py 1Y9yRTntfThrKMJbqyMzVasua25GUucf4 Data/Test_Faster_RCNN_R-50-PFN_2x_VCOCO.pkl
+python lib/utl/Download_data.py 1QI1kcZJqI-ym6AGQ2swwp4CKb39uLf-4 Data/Test_HICO_res101_3x_FPN_hico.pkl
+python lib/utl/Download_data.py 15UXbsoverISJ9wNO-84uI4kQEbRjyRa8 Data/gt_annotations.pkl
+
 python lib/ult/Download_data.py 1le4aziSn_96cN3dIPCYyNsBXJVDD8-CZ Data/Trainval_GT_HICO.pkl
 python lib/ult/Download_data.py 1YrsQUcBEF31cvqgCZYmX5j-ns2tgYXw7 Data/Trainval_GT_VCOCO.pkl
 python lib/ult/Download_data.py 1PPPya4M2poWB_QCoAheStEYn3rPMKIgR Data/Trainval_Neg_HICO.pkl
