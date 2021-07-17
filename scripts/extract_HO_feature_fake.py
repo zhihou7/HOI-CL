844dc62a9a7d2f91ca7119194ba42526e55e923d
zhihou7 <zhou9878@uni.sydney.edu.au>
2021-07-17 17:06:04 +0800

    add fake object t-SNE code

---
+++ scripts/extract_HO_feature_fake.py
@@ -0,0 +1,162 @@
+# --------------------------------------------------------
+# Tensorflow 
+# Licensed under The MIT License [see LICENSE for details]
+# Written by zhihou
+# --------------------------------------------------------
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import _init_paths
+from ult.ult import obtain_data, obtain_test_data
+
+import tensorflow as tf
+import numpy as np
+import argparse
+
+from ult.config import cfg
+from ult.timer import Timer
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Test an iCAN on HICO')
+    parser.add_argument('--num_iteration', dest='iteration',
+            help='Specify which weight to load',
+            default=80000, type=int)
+    parser.add_argument('--model', dest='model',
+            help='Select model',
+            default='iCAN_R_union_multi_base_rew_aug5_3_x5new', type=str)
+    parser.add_argument('--object_thres', dest='object_thres',
+            help='Object threshold',
+            default=0.3, type=float)
+    parser.add_argument('--type', dest='type',
+                        help='Object threshold',
+                        default='train', type=str)
+    parser.add_argument('--human_thres', dest='human_thres',
+            help='Human threshold',
+            default=0.8, type=float)
+
+
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+
+    args = parse_args()
+
+    weight = cfg.LOCAL_DATA + '/Weights/' + args.model + '/HOI_iter_' + str(args.iteration) + '.ckpt'
+
+    print ('Human thres = ' + str(args.human_thres) + ', Object thres = ' + str(args.object_thres) + ', iter = ' + str(args.iteration) + ', path = ' + weight ) 
+  
+    output_file = cfg.LOCAL_DATA + '/Results/' + str(args.iteration) + '_' + args.model + '.pkl'
+
+    HICO_dir = cfg.LOCAL_DATA + '/Results/HICO/' + str(args.iteration) + '_' + args.model + '/'
+
+    tfconfig = tf.ConfigProto(device_count={"CPU": 16},
+                              inter_op_parallelism_threads=8,
+                              intra_op_parallelism_threads=8,
+                              allow_soft_placement=True)
+    # init session
+    # tfconfig = tf.ConfigProto(allow_soft_placement=True)
+    # tfconfig.gpu_options.allow_growth=True
+
+    # else:
+    # tfconfig.gpu_options.per_process_gpu_memory_fraction = 0.15
+    sess = tf.Session(config=tfconfig)
+
+
+    if args.model.__contains__('res101'):
+        os.environ['DATASET'] = 'HICO_res101'
+        from networks.HOI import DisentanglingNet
+        net = DisentanglingNet(model_name=args.model)
+    else:
+        from networks.HOI import DisentanglingNet
+        net = DisentanglingNet(model_name=args.model)
+
+    os.environ['FEATS'] = 'TRUE'
+    if args.type == 'train':
+        large_neg_for_ho = False
+        if args.model.endswith('_aug5_new') or args.model.endswith('_aug6_new'):
+            large_neg_for_ho = True
+        image, image_id, num_pos, Human_augmented, Object_augmented, action_HO, sp, pose_list, obj_mask = obtain_data(
+            Pos_augment=0, Neg_select=0, augment_type=-1, pattern_type=False)
+        net.set_ph(image, image_id, num_pos, Human_augmented, Object_augmented, action_HO, sp, obj_mask)
+        net.set_add_ph(obj_mask)
+    else:
+
+        large_neg_for_ho = False
+        if args.model.endswith('_aug5_new') or args.model.endswith('_aug6_new'):
+            large_neg_for_ho = True
+        image, image_id, num_pos, Human_augmented, Object_augmented, action_HO, sp, obj_mask = obtain_test_data(
+            Pos_augment=0,
+            Neg_select=0,
+            augment_type=-1,
+            pattern_type=False,
+            large_neg_for_ho=large_neg_for_ho)
+        net.set_ph(image, image_id, num_pos, Human_augmented, Object_augmented, action_HO, sp, obj_mask)
+        net.set_add_ph(obj_mask)
+    net.init_verbs_objs_cls()
+    net.create_architecture(False)
+
+    saver = tf.train.Saver()
+    saver.restore(sess, weight)
+
+    print('Pre-trained weights loaded.')
+    detection = {}
+
+    # prediction_HO  = net.test_image_HO(sess, im_orig, blobs)
+    # timers
+    _t = {'im_detect': Timer(), 'misc': Timer()}
+    last_img_id = -1
+    count = 0
+    img_id_list = []
+    O_list = []
+    V_list = []
+    A_list = []
+    result = {}
+    result['img_id_list'] = []
+    result['O_list'] = []
+    result['V_list'] = []
+    result['A_list'] = []
+    # result['fake_O_list'] = []
+    # result['fake_O_all_list'] = []
+    while True:
+        _t['im_detect'].tic()
+
+        try:
+            _image, _image_id, fc7_O, fc7_verbs, fc7_O_fake, fc7_O_fake_all, actions = sess.run(
+                [image, image_id,
+                 net.test_visualize['fc7_O_feats'],
+                 net.test_visualize['fc7_verbs_feats'],
+                 # net.test_visualize['fc7_fake_O_feats'],
+                 tf.constant([0.]),
+                 tf.constant([0.]),
+                 action_HO,
+                ])
+        except tf.errors.OutOfRangeError:
+            print('END')
+            break
+        _t['im_detect'].toc()
+        count += 1
+        if args.model.__contains__('var_gan_gen') or args.model.__contains__('varv_gan_gen'):
+            assert len(fc7_O) == len(fc7_verbs) == len(actions) == len(fc7_O_fake), ( len(fc7_O) ,len(fc7_verbs) ,len(actions) ,len(fc7_O_fake))
+        else:
+            assert len(fc7_O) == len(fc7_verbs) == len(actions), ( len(fc7_O) ,len(fc7_verbs) ,len(actions))
+
+        # print(fc7_O.shape, actions.shape, fc7_O_fake.shape)
+        result['O_list'].extend(fc7_O)
+        result['V_list'].extend(fc7_verbs)
+        # result['fake_O_list'].extend(fc7_O_fake)
+        # result['fake_O_all_list'].extend(fc7_O_fake_all)
+        result['img_id_list'].append([_image_id]*len(fc7_O))
+        result['A_list'].extend(actions)
+        print('im_detect: {:d}/{:d}  {:d}, {:.3f}s'.format(count, 9658, _image_id, _t['im_detect'].average_time))
+
+    print(len(result['A_list']))
+    import pickle
+    pickle.dump(result, open(cfg.LOCAL_DATA+ '/feats/' + args.model +'_'+args.type+'_'+'HICO_HO_feats.pkl', 'wb'))
+    sess.close()
+#
---
+++ scripts/feats_tsne_fake.py
@@ -0,0 +1,243 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/10/10 上午10:01
+# @Author  : Zhi Hou
+# @Site    : 
+# @File    : feats_tsne.py
+# @Software: PyCharm
+#
+#
+#                  .--,       .--,
+#                 ( (  \.---./  ) )
+#                  '.__/o   o\__.'
+#                     {=  ^  =}
+#                      >  -  <
+#                     /       \
+#                    //       \\
+#                   //|   .   |\\
+#                   "'\       /'"_.-~^`'-.
+#                      \  _  /--'         `
+#                    ___)( )(___
+#                   (((__) (__)))
+#              高山仰止,景行行止.虽不能至,心向往之。
+import os
+import numpy as np
+from matplotlib import gridspec
+# import matplotlib.pyplot as plt
+#
+# fig2 = plt.figure(constrained_layout=True)
+# plt.rcParams['figure.figsize'] = (18.0, 9.0)
+# spec2 = gridspec.GridSpec(ncols=3, nrows=2, figure=fig2)
+# f2_ax1 = fig2.add_subplot(spec2[0, 0])
+# f2_ax1.tick_params(direction='out', length=6, width=2)
+# f2_ax2 = fig2.add_subplot(spec2[0, 1])
+# f2_ax3 = fig2.add_subplot(spec2[1, 0])
+# f2_ax4 = fig2.add_subplot(spec2[1, 1])
+# plt.show()
+# exit()
+import _init_paths
+from networks.generate_objects import obtain_hoi_to_obj, visual_tsne, obtain_hoi_to_verb, visual_tsne1, \
+    visual_tsne_multi
+
+if __name__ == '__main__':
+    import matplotlib
+    # print(matplotlib.get_backend())
+
+    import pickle
+
+    # model_name = 'iCAN_R_union_multi_ml1_rew_aug5_3_x5new'
+    # model_name = 'iCAN_R_union_multi_base_rew_aug5_3_x5new'
+    model_name = 'iCAN_R_union_l2_zs7_epic2_s0_7_vloss2_gall_gc_var_gan_dax_rew49_zws_randso2_aug5_3_x5new_res101'
+    import sys
+    if len(sys.argv) > 1:
+        model_name = sys.argv[1]
+    print(model_name)
+    # _train_
+    # feats = pickle.load(open('/opt/data/private/{}_train_HICO_HO_feats.pkl'.format(model_name), 'rb'))
+    if not os.path.exists('/project/ZHIHOU/feats/{}_train_HICO_HO_feats_all.pkl'.format(model_name)):
+        print('you should first extract the feature by extract_HO_feature.py')
+    feats = pickle.load(open('/project/ZHIHOU/feats/{}_train_HICO_HO_feats_all.pkl'.format(model_name), 'rb'))
+
+    hoi_to_obj, obj_names = obtain_hoi_to_obj()
+    hoi_to_verb, verb_names = obtain_hoi_to_verb()
+    feats['O_list'] = [feats['O_list'][i] for i in range(len(feats['A_list'])) if np.sum(feats['A_list'][i]) == 1]
+    feats['fake_O_list'] = [feats['fake_O_list'][i] for i in range(len(feats['A_list'])) if np.sum(feats['A_list'][i]) == 1]
+    feats['A_list'] = [feats['A_list'][i] for i in range(len(feats['A_list'])) if
+                            np.sum(feats['A_list'][i]) == 1]
+    gt_ho = np.asarray(feats['A_list'])
+
+    action_lists = [np.argwhere(a)[0] for a in gt_ho]
+
+    # print(action_lists)
+    obj_lists = [hoi_to_obj[action[0]] for action in action_lists]
+    from collections import Counter
+    obj_counter = Counter(obj_lists)
+    obj_ids = sorted(obj_counter, key=lambda x: obj_counter[x])
+    obj_ids = obj_ids[::-1]
+
+    verb_lists = [hoi_to_verb[action[0]] for action in action_lists]
+    # print(obj_lists)
+    print(len(obj_lists), 'successfully load')
+    new_feats = []
+    new_fake_feats = []
+    n_obj_list = []
+    zs_obj_ids = [4, 23, 28, 30, 33, 50, 52, 58, 59, 67, 71, 79]
+    count_obj = {}
+    obj_num_selected = 10
+    if len(sys.argv) > 2:
+        obj_num_selected = int(sys.argv[2])
+    selected_nums = 100
+    if len(sys.argv) > 3:
+        selected_nums = int(sys.argv[3])
+    selected_obj_ids = obj_ids[:obj_num_selected]
+    verb_lists_list = []
+    zs_obj_lists = []
+    for ii in range(len(obj_lists)):
+        if obj_lists[ii] in selected_obj_ids:
+            if obj_lists[ii] in count_obj:
+                count_obj[obj_lists[ii]] += 1
+            else:
+                count_obj[obj_lists[ii]] = 1
+            if count_obj[obj_lists[ii]] > 100:
+                continue
+            new_feats.append(feats['O_list'][ii])
+            new_fake_feats.append(feats['fake_O_list'][ii])
+            n_obj_list.append(selected_obj_ids.index(obj_lists[ii]))
+            verb_lists_list.append(verb_lists[ii])
+            if obj_lists[ii] in zs_obj_ids:
+                zs_obj_lists.append(1)
+            else:
+                zs_obj_lists.append(0)
+    length = 200000
+    verb_list_ids = list(set(verb_lists_list))
+    verb_lists_list = [verb_list_ids.index(item) for item in verb_lists_list]
+    y2 = ['o'] * min(len(new_feats), length) + ['D'] * min(len(new_fake_feats), length)
+    import matplotlib.pyplot as plt
+
+    f2_ax_list = []
+    if model_name.__contains__('zs'):
+        # plt.rcParams['figure.figsize'] = (8.0, 6.0)
+        plt.rcParams['figure.figsize'] = (18.0, 15.0)
+        fig2 = plt.figure(constrained_layout=True)
+        spec2 = gridspec.GridSpec(ncols=3, nrows=3, figure=fig2)
+        f2_ax1 = fig2.add_subplot(spec2[0, 0])
+        f2_ax1.set_title('(a-1)', fontsize=20)
+        f2_ax2 = fig2.add_subplot(spec2[0, 1])
+        f2_ax2.set_title('(a-2)', fontsize=20)
+        f2_ax3 = fig2.add_subplot(spec2[0, 2])
+        f2_ax3.set_title('(a-3)', fontsize=20)
+
+        f2_ax7 = fig2.add_subplot(spec2[1, 0])
+        f2_ax7.set_title('(b-1)', fontsize=20)
+        f2_ax8 = fig2.add_subplot(spec2[1, 1])
+        f2_ax8.set_title('(b-2)', fontsize=20)
+        f2_ax9 = fig2.add_subplot(spec2[1, 2])
+        f2_ax9.set_title('(b-3)', fontsize=20)
+
+        f2_ax4 = fig2.add_subplot(spec2[2, 0])
+        f2_ax4.set_title('(c-1)', fontsize=20)
+        f2_ax5 = fig2.add_subplot(spec2[2, 1])
+        f2_ax5.set_title('(c-2)', fontsize=20)
+        f2_ax6 = fig2.add_subplot(spec2[2, 2])
+        f2_ax6.set_title('(c-3)', fontsize=20)
+        f2_ax_list = [f2_ax1, f2_ax2, f2_ax3, f2_ax4, f2_ax5, f2_ax6,
+                      f2_ax7, f2_ax8,f2_ax9]
+    else:
+        # plt.rcParams['figure.figsize'] = (8.0, 4.0)
+        plt.rcParams['figure.figsize'] = (18.0, 10.0)
+        fig2 = plt.figure(constrained_layout=True)
+        spec2 = gridspec.GridSpec(ncols=3, nrows=2, figure=fig2)
+        f2_ax1 = fig2.add_subplot(spec2[0, 0])
+        f2_ax1.set_title('(a-1)', fontsize=20)
+        f2_ax2 = fig2.add_subplot(spec2[0, 1])
+        f2_ax2.set_title('(a-2)', fontsize=20)
+        f2_ax3 = fig2.add_subplot(spec2[0, 2])
+        f2_ax3.set_title('(a-3)', fontsize=20)
+        f2_ax4 = fig2.add_subplot(spec2[1, 0])
+        f2_ax4.set_title('(b-1)', fontsize=20)
+        f2_ax5 = fig2.add_subplot(spec2[1, 1])
+        f2_ax5.set_title('(b-2)', fontsize=20)
+        f2_ax6 = fig2.add_subplot(spec2[1, 2])
+        f2_ax6.set_title('(b-3)', fontsize=20)
+        f2_ax_list = [f2_ax1, f2_ax2, f2_ax3, f2_ax4, f2_ax5, f2_ax6]
+    for ax in f2_ax_list:
+        ax.tick_params(labelsize=15)
+    # plt.rcParams['figure.figsize'] = (8.0, 4.0)
+    # plt.clf()
+    visual_tsne(np.asarray(new_feats[:length]), np.asarray(n_obj_list[:length]), obj_num_selected,
+                model_name + 'O_list' + str(obj_num_selected), save_fig=True, old_plt=f2_ax1)
+    visual_tsne(np.asarray(new_fake_feats[:length]), np.asarray(n_obj_list[:length]), obj_num_selected,
+                model_name + 'fake_O_list' + str(obj_num_selected), save_fig=True, old_plt=f2_ax2)
+    visual_tsne1(np.asarray(new_feats[:length] + new_fake_feats[:length]),
+                 np.asarray(n_obj_list[:length] + n_obj_list[:length]), y2, obj_num_selected,
+                 model_name + 'joint_O_list_' + str(obj_num_selected), save_fig=True, old_plt=f2_ax3)
+
+
+    if model_name.__contains__('zs11'):
+        visual_tsne_multi(np.asarray(new_feats[:length]), np.asarray(n_obj_list[:length]),
+                          np.asarray(zs_obj_lists[:length]),
+                          2, obj_num_selected,
+                          model_name + 'O_list_zs_' + str(obj_num_selected), save_fig=True, old_plt=f2_ax7)
+        visual_tsne_multi(np.asarray(new_fake_feats[:length]), np.asarray(n_obj_list[:length]),
+                          np.asarray(zs_obj_lists[:length]),
+                          2, obj_num_selected,
+                    model_name + 'fake_O_list_zs_' + str(obj_num_selected), save_fig=True, old_plt=f2_ax8)
+        # visual_tsne_multi(np.asarray(new_fake_feats[:length]), np.asarray(n_obj_list[:length]),
+        #                   np.asarray(zs_obj_lists[:length]),
+        #                   2, obj_num_selected, model_name + 'Multi_list_zs_' + str(obj_num_selected),
+        #                   save_fig=True)
+        visual_tsne_multi(np.asarray(new_feats[:length] + new_fake_feats[:length]),
+                          np.asarray(n_obj_list[:length] + n_obj_list[:length]),
+                          np.asarray(zs_obj_lists[:length] + zs_obj_lists[:length]), 2, obj_num_selected,
+                          model_name + 'joint_O_list_zs_' + str(obj_num_selected), save_fig=True, old_plt=f2_ax9)
+
+    visual_tsne_multi(np.asarray(new_feats[:length]), np.asarray(n_obj_list[:length]), np.asarray(verb_lists_list[:length]),
+                      len(verb_list_ids), obj_num_selected, model_name + 'Multi_list'+str(obj_num_selected), save_fig=True, old_plt=f2_ax4)
+    visual_tsne_multi(np.asarray(new_fake_feats[:length]), np.asarray(n_obj_list[:length]),
+                      np.asarray(verb_lists_list[:length]),
+                      len(verb_list_ids), obj_num_selected, model_name + 'Multi_list_fake_' + str(obj_num_selected),
+                      save_fig=True, old_plt=f2_ax5)
+    visual_tsne_multi(np.asarray(new_feats[:length] + new_fake_feats[:length]),
+                      np.asarray(n_obj_list[:length] + n_obj_list[:length]),
+                      np.asarray(verb_lists_list[:length] + verb_lists_list[:length]),
+                      len(verb_list_ids),
+                      obj_num_selected,
+                      model_name + 'Multi_joint_O_list_' + str(obj_num_selected), save_fig=True, old_plt=f2_ax6)
+
+    fig2.savefig('/project/ZHIHOU/jpg_test/{}.pdf'.format(model_name), dpi=300)
+    exit()
+    num_inst = np.load("/data1/zhihou/num_inst.npy")
+    rare_idx = np.where(num_inst <= 10)[0].tolist()
+    num_threhold = 50
+    rare_feats = []
+    action_gt = []
+    num_classes = 80
+    count_key = [0 for i in range(num_classes)]
+    for i in range(len(action_lists)):
+        objs = set([hoi_to_obj[action_lists[i][j]] for j in range(len(action_lists[i]))])
+        if len(objs) > 1:
+            continue
+        if hoi_to_obj[action_lists[i][0]] < num_classes and count_key[hoi_to_obj[action_lists[i][0]]] < num_threhold:
+            rare_feats.append(feats['O_list'][i])
+            action_gt.append(hoi_to_obj[action_lists[i][0]])
+            count_key[hoi_to_obj[action_lists[i][0]]] += 1
+            # action_gt.append(rare_idx.index(action_lists[i][0]))
+
+    old_gt = action_gt
+    old_feats = rare_feats
+    action_gt = []
+    rare_feats = []
+    label_select = [ll for ll in list(range(num_classes)) if count_key[ll] >= num_threhold]
+    label_select = [ll for ll in list(range(num_classes))]
+
+    for i in range(len(old_gt)):
+        if old_gt[i] in label_select:
+            action_gt.append(label_select.index(old_gt[i]))
+            rare_feats.append(old_feats[i])
+
+    print(count_key, len(action_lists))
+    print(count_key, len(action_lists), len(count_key), label_select, len(label_select))
+    print(np.argwhere(np.asarray(count_key) < num_threhold))
+    print(len(np.asarray(rare_feats)), '========', len(np.asarray(action_gt)))
+    visual_tsne(np.asarray(rare_feats), np.asarray(action_gt),
+                label_nums=len(label_select), title='o/'+model_name + 'O1_list_act_'+str(num_threhold)+'_117', save_fig=True)
